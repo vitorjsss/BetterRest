f4a8bcd64da44a1ba9e70d44a342f31aa24f02cf
Vitor <vitorjsssmamede.1@gmail.com>
2024-04-09 13:20:22 -0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Vitor on 09/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
